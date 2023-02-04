f3381239999778e2e3e039f5f201cc0b10687550
rohitgarud <rohit.garuda1992@gmail.com>
2023-02-04 12:38:23 +0530

    Add utility functions cleaning doi, get output path

---
+++ asreviewcontrib/preprocess/preprocess_utils.py
@@ -0,0 +1,28 @@
+import re
+import os
+from datetime import datetime
+
+
+def clean_doi(doi):
+    pattern = re.compile(r"(https?://(dx.)?doi\.org/)?(10.\S+)")
+    return f"https://doi.org/{pattern.match(doi).group(3)}"
+
+
+def clean_authors(authors):
+    pass
+
+
+def get_output_path(args):
+    input_path = args.input_path[0]
+    if args.output_path:
+        output_path = args.output_path
+        if not output_path.endswith(".csv"):
+            if "." in output_path:
+                raise ValueError(
+                    "File extensions other than .csv are not supported yet"
+                )
+            else:
+                output_path += ".csv"
+    else:
+        output_path = os.path.basename(input_path)
+        output_path = f"{os.path.splitext(output_path)[0]}-deduplicated-{datetime.now().strftime('%Y%m%dT%H%M')}.csv"
