0682d517a4da5a681d6a426bfba528aa597a413d
rohitgarud <rohit.garuda1992@gmail.com>
2023-02-20 20:47:54 +0530

    add Citationkey, and update records functionality
    Add Citationkey feature for blocking

--- asreviewcontrib/preprocess/preprocess_utils.py
+++ asreviewcontrib/preprocess/preprocess_utils.py
@@ -7,12 +7,16 @@ import csv
 import pandas as pd
 import numpy as np
 from unidecode import unidecode
+from pyalex import Works
+from tinydb import TinyDB, Query
 
 from asreview.config import COLUMN_DEFINITIONS
 from asreview.exceptions import BadFileFormatError
 from asreview.io.utils import _standardize_dataframe, type_from_column
 from asreview.data import load_data as asreview_load_data
+
 from xml_reader import EndnoteXMLReader
+
 
 # Additional column definitions required for deduplication
 DEDUPLICATION_COLUMN_DEFINITIONS = {
@@ -43,6 +47,8 @@ HTML_ENTITIES = [
     "&copy;",
     "&reg;",
 ]
+
+OPENALEX_QUERY_LIMIT = 25
 
 # Dictionary of journal name abbreviations and full forms
 with open("all_journal_abbreviations.csv", "r") as f:
@@ -161,6 +167,29 @@ def clean_isbn(isbn):
     return isbn
 
 
+def get_first_author(authors):
+    first_author = []
+    for item in authors.split():
+        if len(item) == 1:
+            break
+        first_author.append(item)
+    first_author = "".join(first_author)
+    first_author = re.sub(r"[^A-Za-z]", "", first_author)
+    return first_author.upper()
+
+
+def get_short_title(title):
+    short_title = []
+    for word in title.split():
+        if len(word) > 2:
+            short_title.append(word)
+        if len(short_title) == 3:
+            break
+    short_title = "".join(short_title)
+    short_title = re.sub(r"[^A-Za-z]", "", short_title)
+    return short_title.upper()
+
+
 def get_output_path(args):
     """Get output path based on user input.
 
@@ -272,3 +301,143 @@ def _standardize_dataframe_for_deduplication(df, column_spec={}):
     )
 
     return df, all_column_spec
+
+
+def retrieve_records_from_localdb(doi_list):
+    doi_list_to_retrieve = []
+    locally_retrieved_records = {}
+
+    db = TinyDB("./records.json")
+    Record = Query()
+    for doi in doi_list:
+        try:
+            metadata = db.search(Record.doi == doi)[0]
+            locally_retrieved_records[doi] = metadata
+        except IndexError:
+            doi_list_to_retrieve.append(doi)
+
+    return locally_retrieved_records, doi_list_to_retrieve
+
+
+def update_localdb(retrieved_records, doi_list):
+    db = TinyDB("./records.json")
+
+    for doi in doi_list:
+        try:
+            db.insert(retrieved_records[doi])
+        except KeyError:
+            pass
+
+
+def retrieve_records(doi_list):
+    # Try to retrieve records from local database if available
+    retrieved_records, doi_list = retrieve_records_from_localdb(doi_list)
+
+    # doi_chunks = (
+    #     doi_list[i : i + OPENALEX_QUERY_LIMIT]
+    #     for i in range(0, len(doi_list), OPENALEX_QUERY_LIMIT)
+    # )
+    # # Retrieve records in chunks using OpenAlex "OR" syntax with pyalex
+    # # TODO: Check if Lens.org api or database can be used freely
+
+    # for chunk in doi_chunks:
+    #     data_chunk = Works().filter(doi="|".join(chunk)).get()
+    #     for data in data_chunk:
+    #         retrieved_records[data["doi"]] = data
+
+    # update_localdb(retrieved_records, doi_list)
+
+    return retrieved_records
+
+
+def parse_metadata(retrieved_records):
+    # Parse metadata in JSON format and create pandas dataframe with required columns
+    parsed_data = []
+    for doi, record in retrieved_records.items():
+        title = record.get("title")
+        authors = ", ".join(
+            author["author"]["display_name"] for author in record.get("authorships", [])
+        )
+        year = record.get("publication_year")
+        abstract = record.get("abstract")
+        try:
+            if record["biblio"].get("first_page"):
+                pages = (
+                    f"{record['biblio']['first_page']}-{record['biblio']['last_page']}"
+                )
+            else:
+                pages = None
+        except KeyError:
+            pages = None
+        volume = record["biblio"].get("volume")
+        number = record["biblio"].get("issue")
+
+        # TODO: Get Journal and ISBN
+
+        metadata = {
+            "title": title,
+            "authors": authors,
+            "year": year,
+            "abstract": abstract,
+            "pages": pages,
+            "volume": volume,
+            "number": number,
+            "doi": doi,
+        }
+        parsed_data.append(metadata)
+    parsed_data_df = pd.DataFrame(parsed_data)
+    return parsed_data_df
+
+
+def update_records(records_df, col_specs):
+    """Find missing information from OpenAlex API using pyalex
+
+    Parameters
+    ----------
+    records_df : pandas dataframe
+        Dataframe of citation records
+    col_specs : dict
+        Column definitions for mapping column names to standardized names
+    """
+    # Clean dois in case not already cleaned as they will be used to retrieve record metadata
+    records_df[col_specs["doi"]] = records_df[col_specs["doi"]].apply(clean_doi)
+
+    # Make year column string type
+    records_df["year"] = records_df["year"].astype("object")
+
+    # Make missing values as NAN
+    string_columns = records_df.select_dtypes("object").columns
+    records_df[string_columns] = (
+        records_df[string_columns]
+        .fillna("")
+        .applymap(lambda val: np.nan if len(val) == 0 else val)
+    )
+
+    # Find records where DOI is available and fields required for deduplication are missing
+    records_df["missing_data"] = (
+        records_df[col_specs["title"]].isna()
+        | records_df[col_specs["authors"]].isna()
+        | records_df[col_specs["abstract"]].isna()
+        | records_df[col_specs["year"]].isna()
+        | records_df[col_specs["journal"]].isna()
+        | records_df[col_specs["doi"]].isna()
+        | records_df[col_specs["pages"]].isna()
+        | records_df[col_specs["volume"]].isna()
+        | records_df[col_specs["number"]].isna()
+        | records_df[col_specs["isbn"]].isna()
+    ) & records_df[col_specs["doi"]].notna()
+
+    doi_list = records_df[col_specs["doi"]][records_df["missing_data"]].values
+    retrieved_records = retrieve_records(doi_list)
+    retrieved_records_df = parse_metadata(retrieved_records)
+
+    retrieved_records_df = (
+        records_df[col_specs["doi"]]
+        .reset_index()
+        .merge(retrieved_records_df, on="doi")
+        .set_index("record_id")
+    )
+
+    updated_records_df = records_df.combine_first(retrieved_records_df)
+
+    return updated_records_df
