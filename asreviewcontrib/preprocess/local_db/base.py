2ce7b6591acb1ba3dba14734fc50da81aa7b00e7
rohitgarud <rohit.garuda1992@gmail.com>
2023-03-07 21:49:49 +0530

    Refactor localdb manager

--- asreviewcontrib/preprocess/local_db/base.py
+++ asreviewcontrib/preprocess/local_db/base.py
@@ -7,12 +7,13 @@ class BaseLocalDB(ABC):
     name = "base"
 
     @abstractmethod
-    def retrieve_records_from_localdb(self, doi_list):
+    def retrieve_records(self, doi_list):
         """Retrieve records from local database from doi list"""
 
         raise NotImplementedError
 
-    def update_localdb(self, retrieved_records, doi_list):
+    @abstractmethod
+    def add_records(self, retrieved_records, doi_list):
         """Add retrieved records to local database"""
 
         raise NotImplementedError
--- asreviewcontrib/preprocess/local_db/localdb.py
+++ asreviewcontrib/preprocess/local_db/localdb.py
@@ -3,13 +3,27 @@ from asreviewcontrib.preprocess.local_db.base import BaseLocalDB
 
 
 class TinyLocalDB(BaseLocalDB):
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         super(TinyLocalDB, self).__init__()
-        self.db = TinyDB("./records.json")
+        self.db_path = self._get_localdb_path()
+        self.db = TinyDB(self.db_path)
 
-    def retrieve_records_from_localdb(self, doi_list):
+    def retrieve_records(self, doi_list):
+        """Retrieve records corresponding to dois in the doi_list
+        from the local datatabase
+
+        Parameters
+        ----------
+        doi_list : list
+            list of dois in unified format
+
+        Returns
+        -------
+        tuple
+            tuple of:
+            locally_retrieved_records (records available in localdb) and
+            doi_list_to_retrieve (dois of records not available in localdb)
+        """
         doi_list_to_retrieve = []
         locally_retrieved_records = {}
 
@@ -23,10 +37,26 @@ class TinyLocalDB(BaseLocalDB):
 
         return locally_retrieved_records, doi_list_to_retrieve
 
-    def update_localdb(self, retrieved_records, doi_list):
+    def add_records(self, retrieved_records, doi_list):
+        """Add records retrieved from OpenAlex API to local database
 
+        Parameters
+        ----------
+        retrieved_records : dict
+            Dictionary of retrieved records with dois as keys
+        doi_list : list
+            List of dois
+        """
         for doi in doi_list:
             try:
                 self.db.insert(retrieved_records[doi])
             except KeyError:
                 pass
+
+    def _get_localdb_path(self):
+        localdb_path = "./records.json"
+        # TODO: Save local database in safe location for future use
+        return localdb_path
+
+
+# TODO: Add classes for other NoSQL databases like MongoDB and ElasticSearch
